ae0b0c45ba881e00a5cca918794dc103c350319e
SThouvenin <Shannon.Thouvenin@gmail.com>
2017-11-22 14:15:18 -0500

    Gallery completed

--- js/galleryjs.js
+++ js/galleryjs.js
@@ -1,26 +1,43 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var sliderObjects = [];
+createSliderObjects();
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusDivs(obj, n) {
+  var parentDiv = $(obj).parent();
+  var matchedDiv;
+  $.each(sliderObjects, function(i, item) {
+    if ($(parentDiv[0]).attr('id') == $(item).attr('id')) {
+      matchedDiv = item;
+      return false;
+    }
+  });
+  matchedDiv.slideIndex=matchedDiv.slideIndex+n;
+  showDivs(matchedDiv, matchedDiv.slideIndex);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function createSliderObjects() {
+  var sliderDivs = $('.slider');
+  $.each(sliderDivs, function(i, item) {
+    var obj = {};
+    obj.id = $(item).attr('id');
+    obj.divContent = item;
+    obj.slideIndex = 1;
+    obj.slideContents = $(item).find('.mySlides');
+    showDivs(obj, 1);
+    sliderObjects.push(obj);
+  });
 }
 
-function showSlides(n) {
+function showDivs(divObject, n) {
+  debugger;
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+  if (n > divObject.slideContents.length) {
+    divObject.slideIndex = 1
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  if (n < 1) {
+    divObject.slideIndex = divObject.slideContents.length
   }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  for (i = 0; i < divObject.slideContents.length; i++) {
+    divObject.slideContents[i].style.display = "none";
+  }
+  divObject.slideContents[divObject.slideIndex - 1].style.display = "block";
 }
